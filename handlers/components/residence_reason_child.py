07a10d067b945061ed91be995341152da9a6f197
johngoworks <johngoworks@gmail.com>
2025-08-14 23:19:45 +0500

    Add residence reason handling for child; implement state management and keyboard options

--- handlers/components/residence_reason_child.py
+++ handlers/components/residence_reason_child.py
@@ -2,24 +2,30 @@ from aiogram import Router, F
 from aiogram.types import CallbackQuery, Message
 from aiogram.fsm.context import FSMContext
 
-from keyboards.components.residence_reason_marriage import (
+
+from keyboards.components.residence_reason_child import (
     get_residence_reason_photo_or_manual_keyboard,
+    get_residence_reason_who_for_child_keyboard,
 )
-from states.components.residence_reason_marriage import ResidenceReasonMarriageStates
+from states.components.residence_reason_child import ResidenceReasonChildStates
+
 
 from localization import _
 from data_manager import SecureDataManager
 
-residence_reason_marriage_router = Router()
+
+residence_reason_child_router = Router()
 data_manager = SecureDataManager()
 
 
-@residence_reason_marriage_router.callback_query(F.data == "residence_reason_marriage")
-async def handle_residence_reason_marriage(callback: CallbackQuery, state: FSMContext):
+@residence_reason_child_router.callback_query(F.data == "residence_reason_child")
+async def handle_residence_reason_patent(callback: CallbackQuery, state: FSMContext):
     """Handle the selection of residence reason patent input method."""
 
     # Set the state for choosing photo or manual input
-    await state.set_state(ResidenceReasonMarriageStates.choose_photo_or_manual)
+    await state.set_state(ResidenceReasonChildStates.choose_photo_or_manual)
+    await state.update_data(residence_reason="residence_reason_child")
+
     state_data = await state.get_data()
     lang = state_data.get("language")
 
@@ -30,71 +36,115 @@ async def handle_residence_reason_marriage(callback: CallbackQuery, state: FSMCo
     )
 
 
-@residence_reason_marriage_router.callback_query(
-    F.data == "start_residence_reason_marriage_manual"
+@residence_reason_child_router.callback_query(
+    F.data == "start_residence_reason_child_manual"
 )
 async def handle_start_manual(callback: CallbackQuery, state: FSMContext):
-    """Handle the start of manual input for residence reason marriage."""
+    """Handle the start of manual input for residence reason child."""
 
     # Set the state for manual input
-    await state.set_state(ResidenceReasonMarriageStates.spouse_fio)
+    await state.set_state(ResidenceReasonChildStates.child_fio)
     state_data = await state.get_data()
     lang = state_data.get("language")
 
-    # Send a message to start manual input
-    text = f"{_.get_text('residence_reason_manual_marriage_messages.full_name.title', lang)}\n\n{_.get_text('residence_reason_manual_marriage_messages.full_name.description', lang)}\n{_.get_text('residence_reason_manual_marriage_messages.full_name.example_text', lang)}"
+    # Send a message with options for who the residence reason is for child
+    text = f"{_.get_text("residence_reason_manual_child_messages.child_fio.title", lang)}\n"
+    text += f"\n{_.get_text('residence_reason_manual_child_messages.child_fio.description', lang)}\n\n"
+    text += f"{_.get_text('residence_reason_manual_child_messages.child_fio.name_text', lang)}\n"
+    text += f"{_.get_text('residence_reason_manual_child_messages.child_fio.example_text', lang)}"
+
     await callback.message.edit_text(text=text)
 
 
-@residence_reason_marriage_router.message(ResidenceReasonMarriageStates.spouse_fio)
-async def get_spouse_fio(message: Message, state: FSMContext):
-    """Handle the input of spouse full name for residence reason marriage."""
-    spouse_fio = message.text.strip()
-    # Set the state for spouse full name input
-    await state.set_state(ResidenceReasonMarriageStates.issue_date)
-    await state.update_data(spouse_fio=spouse_fio)
+@residence_reason_child_router.message(ResidenceReasonChildStates.child_fio)
+async def get_child_fio(message: Message, state: FSMContext):
+    """Handle the input of child's full name for residence reason child."""
+    child_fio = message.text.strip()
+    await state.update_data(child_fio=child_fio)
+
+    # Set the state for child's birth date input
+    await state.set_state(ResidenceReasonChildStates.child_birth_date)
     state_data = await state.get_data()
     lang = state_data.get("language")
 
-    # Prompt the user to enter their spouse's birth date
-    text = f"{_.get_text("residence_reason_manual_marriage_messages.marriage_date.title", lang)}\n{_.get_text("residence_reason_manual_marriage_messages.marriage_date.example_text", lang)}"
+    # Prompt the user to enter child's birth date
+    text = f"{_.get_text('residence_reason_manual_child_messages.child_birth_date.title', lang)}\n"
+    text += f"{_.get_text('residence_reason_manual_child_messages.child_birth_date.example_text', lang)}"
     await message.answer(text=text)
 
 
-@residence_reason_marriage_router.message(ResidenceReasonMarriageStates.issue_date)
-async def get_issue_date(message: Message, state: FSMContext):
-    """Handle the input of issue date for residence reason marriage."""
-    issue_date = message.text.strip()
-    # Set the state for issue date input
-    await state.set_state(ResidenceReasonMarriageStates.marriage_number)
-    await state.update_data(issue_date=issue_date)
+@residence_reason_child_router.message(ResidenceReasonChildStates.child_birth_date)
+async def get_child_birth_date(message: Message, state: FSMContext):
+    """Handle the input of child's birth date for residence reason child."""
+    child_birth_date = message.text.strip()
+    await state.update_data(child_birth_date=child_birth_date)
+
+    # Set the state for child's citizenship input
+    await state.set_state(ResidenceReasonChildStates.child_citizenship)
     state_data = await state.get_data()
     lang = state_data.get("language")
 
-    # Prompt the user to enter their marriage number
-    text = f"{_.get_text('residence_reason_manual_marriage_messages.marriage_certificate.title', lang)}\n{_.get_text('residence_reason_manual_marriage_messages.marriage_certificate.example_text', lang)}"
+    # Prompt the user to enter child's citizenship
+    text = f"{_.get_text('residence_reason_manual_child_messages.child_citizenship.title', lang)}\n"
+    text += f"{_.get_text('residence_reason_manual_child_messages.child_citizenship.example_text', lang)}"
     await message.answer(text=text)
 
 
-@residence_reason_marriage_router.message(ResidenceReasonMarriageStates.marriage_number)
-async def get_marriage_number(message: Message, state: FSMContext):
-    """Handle the input of marriage number for residence reason marriage."""
-    marriage_number = message.text.strip()
-    # Set the state for marriage number input
-    await state.set_state(ResidenceReasonMarriageStates.issue_place)
-    await state.update_data(marriage_number=marriage_number)
+@residence_reason_child_router.message(ResidenceReasonChildStates.child_citizenship)
+async def get_child_citizenship(message: Message, state: FSMContext):
+    """Handle the input of child's citizenship for residence reason child."""
+    child_citizenship = message.text.strip()
+    await state.update_data(child_citizenship=child_citizenship)
+
+    # Set the state for child's certificate number input
+    await state.set_state(ResidenceReasonChildStates.child_certificate_number)
     state_data = await state.get_data()
     lang = state_data.get("language")
 
-    # Prompt the user to enter the place of issue
-    text = f"{_.get_text('residence_reason_manual_marriage_messages.marriage_issue_place.title', lang)}\n{_.get_text('residence_reason_manual_marriage_messages.marriage_issue_place.example_text', lang)}"
+    # Prompt the user to enter child's certificate number
+    text = f"{_.get_text('residence_reason_manual_child_messages.child_birth_cert_number.title', lang)}\n"
+    text += f"{_.get_text('residence_reason_manual_child_messages.child_birth_cert_number.example_text', lang)}"
+
     await message.answer(text=text)
-    await state.update_data(waiting_data="marriage_issue_place")
-    next_states = state_data.get("next_states", [])
-    from_action = state_data.get("from_action")
-    if len(next_states) > 0:
-        next_state = next_states[0]
-        await state.set_state(next_state)
-    else:
-        # If no next states, return to the previous action
-        await state.set_state(from_action)
+
+
+@residence_reason_child_router.message(
+    ResidenceReasonChildStates.child_certificate_number
+)
+async def get_child_certificate_number(message: Message, state: FSMContext):
+    """Handle the input of child's certificate number for residence reason child."""
+    child_certificate_number = message.text.strip()
+    await state.update_data(child_certificate_number=child_certificate_number)
+
+    # Set the state for child's certificate issue place input
+    await state.set_state(ResidenceReasonChildStates.child_certificate_issue_place)
+    state_data = await state.get_data()
+    lang = state_data.get("language")
+
+    # Prompt the user to enter child's certificate issue place
+    text = f"{_.get_text('residence_reason_manual_child_messages.child_birth_cert_issue_place.title', lang)}\n"
+    text += f"{_.get_text('residence_reason_manual_child_messages.child_birth_cert_issue_place.example_text', lang)}"
+
+    await message.answer(text=text)
+
+
+@residence_reason_child_router.message(
+    ResidenceReasonChildStates.child_certificate_issue_place
+)
+async def get_child_certificate_issue_place(message: Message, state: FSMContext):
+    """Handle the input of child's certificate issue place for residence reason child."""
+    child_certificate_issue_place = message.text.strip()
+    await state.update_data(child_certificate_issue_place=child_certificate_issue_place)
+
+    # Set the state for choosing who the residence reason is for child
+    await state.set_state(ResidenceReasonChildStates.who_for_child)
+    state_data = await state.get_data()
+    lang = state_data.get("language")
+
+    # Send a message with options for who the residence reason is for child
+    text = _.get_text(
+        "residence_reason_manual_child_messages.who_for_child.description", lang
+    )
+    await message.answer(
+        text=text, reply_markup=get_residence_reason_who_for_child_keyboard(lang)
+    )
