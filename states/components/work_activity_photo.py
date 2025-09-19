460411638e4cfc9a791a393b3ad53116d3502dc7
Wirexiam <alfe2230@gmail.com>
2025-09-19 18:55:11 +0300

    chore: удалить временные OCR-файлы для work_activity

--- states/components/work_activity_photo.py
+++
@@ -1,5 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class WorkActivityPhotoStates(StatesGroup):
-    waiting_passport_or_patent_photo = State()  # ждём фото (паспорт/патент)
-    preview = State()                            # показ предпросмотра OCR-данных
